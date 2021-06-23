a11890753ce3a40ac4283bf1199e2bc87fc093b5
root <e.abiynur@gmail.com>
2021-06-23 03:15:36 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,14 @@
 
 /**
  * print_alphabet - Prints the alphabet,in lower case
- * 
+ *
  * Return 0
  */
 void print_alphabet(void)
 {
 	int x;
 
-	for(x = 0; x <= 26 ; x++)
+	for (x = 0; x < 26 ; x++)
 	{
 		_putchar(x);
 	}
