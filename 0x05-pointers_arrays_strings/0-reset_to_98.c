4de2ccaf21d3a0babfb1759e97470f0f93171ca4
root <e.abiynur@gmail.com>
2021-06-28 01:39:33 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - function that prints to an int as parameter, updates value
+ * @n: int
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,4 @@
+#define HOLBERTON_H
+
+int _putchar(charc c);
+void reset_to_98(int *n);
