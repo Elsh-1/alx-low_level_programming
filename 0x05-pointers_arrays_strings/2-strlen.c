75b531f3ac450146722273def95c061a9906c89b
root <e.abiynur@gmail.com>
2021-06-28 22:20:01 -0700

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _strlen - the lenght of a string
+ * @s: pointer to the string
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[1])
+
+		i++;
+
+	return (i);
+}
