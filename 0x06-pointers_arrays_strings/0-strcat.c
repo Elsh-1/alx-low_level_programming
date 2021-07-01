2e612f7130edd3623a303d734506f1b19d05ecff
root <e.abiynur@gmail.com>
2021-07-01 00:07:24 -0700

    concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *_strcat - - funciton that concatenates two strings
+ * @dest:  destinaiton
+ * @src: source
+ * Return: string
+ */
+char *_strcat(char *dest. char *src)
+{
+	char *dest;
+	char *src;
+
+	{
+		_strcat(dest, src);
+	}
+	return 0;
+}
+
+
+
