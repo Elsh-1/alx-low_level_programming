cd31c48199d7c816b73c3cf5de8f0a2421fb2d76
root <e.abiynur@gmail.com>
2021-07-01 02:09:30 -0700

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,14 +7,15 @@
  */
 char *_strcat(char *dest. char *src)
 {
-	char *dest;
-	char *src;
+int a = -1, i;
+for (i = 0; dest[i] != '\0'; i++)
+;
 
-	{
-		_strcat(dest, src);
-	}
-	return 0;
+do {
+	a++;
+	dest[i] = src[a];
+	i++;
+} while (src[a] != '\0');
+
+return (dest);
 }
-
-
-
