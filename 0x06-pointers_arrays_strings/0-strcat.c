5efbb999e3e7bf2255150f9dd732f9e49008733f
root <e.abiynur@gmail.com>
2021-07-01 02:15:08 -0700

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 /**
- *_strcat - - funciton that concatenates two strings
- * @dest:  destinaiton
+ *_strcat - funciton that concatenates two strings
+ * @dest: destinaiton
  * @src: source
  * Return: string
  */
-char *_strcat(char *dest. char *src)
+char *_strcat(char *dest, char *src)
 {
 int a = -1, i;
 for (i = 0; dest[i] != '\0'; i++)
