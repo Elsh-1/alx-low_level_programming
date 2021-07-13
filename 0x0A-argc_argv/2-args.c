d8497859c3835017849ac34063adef40dcbca830
root <e.abiynur@gmail.com>
2021-07-13 11:14:16 -0700

    argss

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - program that prints its name
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc , char *argv[])
+{
+	for (count = 0; count < argc; count++)
+	{
+		printf("%s\n", count, argv[count]);
+	return (0);
+}
+
